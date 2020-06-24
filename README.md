a917cbd3bca5344d3724565abe68e62d46468b3d
hankhank10 <m.a.hankinson@gmail.com>
2020-06-24 09:32:52 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # simplenfc
  
+Super simple test script to read the contents of an nfc tag and print it to the screen
--- readnfc.py
+++ readnfc.py
@@ -1,18 +1,14 @@
 import time
 import nfc
-import requests
 
 # this function gets called when a NFC tag is detected
 def touched(tag):
 
     if tag.ndef:
         for record in tag.ndef.records:
-            urltoget = record.text
+            receivedtext = record.text
 
             print("Read from NFC tag: "+ receivedtext)
-            print ("Fetching URL via HTTP: "+ urltoget)
-            r = requests.get(urltoget)
-            print ("")
 
     else:
         print ("Tag Misread - Sorry")
@@ -28,4 +24,4 @@ print("")
 
 while True:
     reader.connect(rdwr={'on-connect': touched})
-    time.sleep(0.1);
+    time.sleep(0.1)
